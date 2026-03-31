9782b6b91451f2993a750aceae653fd9b7640267
Lai Khang Duy <laiduy98@gmail.com>
2026-03-31 22:11:53 +0200

    chore: add DRF, django-filter, and create todos app
    - Install djangorestframework and django-filter
    - Create todos Django app with tests package
    - Add rest_framework, rest_framework.authtoken, django_filters, todos to INSTALLED_APPS
    - Configure DRF defaults: TokenAuth, IsAuthenticated, filtering, pagination

--- autowork/autowork/settings.py
+++ autowork/autowork/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'todos',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +119,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
