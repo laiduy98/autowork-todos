e2eb14e8fc1896d02643a0cdced0fff83a0fd1ff
Lai Khang Duy <laiduy98@gmail.com>
2026-03-31 22:18:52 +0200

    feat: register Category and Todo models in Django admin

--- autowork/todos/admin.py
+++ autowork/todos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from todos.models import Category, Todo
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "created_at")
+    search_fields = ("name",)
+    list_filter = ("user",)
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "priority", "user", "due_date", "is_deleted", "created_at")
+    list_filter = ("status", "priority", "is_deleted", "user")
+    search_fields = ("title", "description")
